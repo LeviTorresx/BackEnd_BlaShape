70583d3bff2e7aec36449caf04faeadd0de262b0
LeviTorresx <levist42@gmail.com>
2025-09-02 17:50:46 -0500

    [001] New: base project for the backend

---
+++ src/main/java/com/blashape/backend_blashape/BackendBlashapeApplication.java
@@ -0,0 +1,13 @@
+package com.blashape.backend_blashape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBlashapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBlashapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blashape/backend_blashape/BackendBlashapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blashape.backend_blashape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBlashapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
