79a8378fdefaea3f9e84840b7c04016395f60c17
Jwhitegomez <jwhite.gomez@udea.edu.co>
2025-10-13 15:26:08 -0500

    [004] Automatic alert method and API creation

--- src/main/java/com/blashape/backend_blashape/repositories/FurnitureRepository.java
+++ src/main/java/com/blashape/backend_blashape/repositories/FurnitureRepository.java
@@ -2,8 +2,15 @@ package com.blashape.backend_blashape.repositories;
 
 import com.blashape.backend_blashape.entitys.Furniture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface FurnitureRepository extends JpaRepository<Furniture, Long> {
+
+    @Query("SELECT f FROM Furniture f WHERE f.endDate BETWEEN :today AND :futureDate")
+    List<Furniture> findByEndDateAndFutureDate(LocalDate today, LocalDate futureDate);
 }
