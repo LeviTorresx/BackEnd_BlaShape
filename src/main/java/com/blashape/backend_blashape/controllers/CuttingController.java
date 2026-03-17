88084f7f80ff4d1a624b93d7546a44df9293ade2
LeviTorresx <Levist42@gmail.com>
2026-03-16 20:43:33 -0500

    [005] delete: Borrado de logica de corte antigua

--- src/main/java/com/blashape/backend_blashape/controllers/CuttingController.java
+++ src/main/java/com/blashape/backend_blashape/controllers/CuttingController.java
@@ -1,14 +1,6 @@
 package com.blashape.backend_blashape.controllers;
 
-import com.blashape.backend_blashape.DTOs.CuttingDTO;
-import com.blashape.backend_blashape.DTOs.SvgRequest;
-import com.blashape.backend_blashape.DTOs.SvgResponse;
-import com.blashape.backend_blashape.services.CuttingService;
-import com.blashape.backend_blashape.services.GuillotineAlgorithm;
-import com.blashape.backend_blashape.services.SvgPreviewGenerator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,36 +9,5 @@ import java.util.List;
 @RequestMapping("/api_BS/cutting")
 @RequiredArgsConstructor
 public class CuttingController {
-    private final CuttingService cuttingService;
 
-    @PostMapping("/preview")
-    public SvgResponse generatePreview(@RequestBody SvgRequest request) {
-        return cuttingService.createCuttingPreview(request);
-    }
-
-    @GetMapping(value = "/preview/{sheet}", produces = "image/svg+xml")
-    public String previewSheet(
-            @PathVariable int sheet,
-            @RequestBody SvgRequest request
-    ) {
-
-        GuillotineAlgorithm.PackingResult result = GuillotineAlgorithm.pack(
-                request.getContainerWidth(),
-                request.getContainerHeight(),
-                request.getItems(),
-                request.getKerf()
-        );
-
-        List<String> svgs = SvgPreviewGenerator.generateAll(
-                result.sheets(),
-                result.wastePercents(),
-                request.getContainerWidth(),
-                request.getContainerHeight(),
-                request.getKerf(),
-                request.getPreviewWidth(),
-                request.getPreviewHeight()
-        );
-
-        return svgs.get(sheet);
-    }
 }
--- src/main/java/com/blashape/backend_blashape/services/CuttingService.java
+++ src/main/java/com/blashape/backend_blashape/services/CuttingService.java
@@ -1,55 +1,10 @@
 package com.blashape.backend_blashape.services;
 
-import com.blashape.backend_blashape.DTOs.SvgRequest;
-import com.blashape.backend_blashape.DTOs.SvgResponse;
-import com.blashape.backend_blashape.config.JwtUtil;
-import com.blashape.backend_blashape.mapper.CuttingMapper;
-import com.blashape.backend_blashape.mapper.PieceMapper;
-import com.blashape.backend_blashape.repositories.CarpenterRepository;
-import com.blashape.backend_blashape.repositories.CuttingRepository;
-import com.blashape.backend_blashape.repositories.FurnitureRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CuttingService {
-
-    private final CuttingRepository cuttingRepository;
-    private final FurnitureRepository furnitureRepository;
-    private final CuttingMapper cuttingMapper;
-    private final CarpenterRepository carpenterRepository;
-    private final JwtUtil jwtUtil;
-    private final PieceMapper pieceMapper;
-
-    @Transactional
-    public SvgResponse createCuttingPreview(SvgRequest request) {
-
-        GuillotineAlgorithm.PackingResult result = GuillotineAlgorithm.pack(
-                request.getContainerWidth(),
-                request.getContainerHeight(),
-                request.getItems(),
-                request.getKerf()
-        );
-
-        List<String> svgs = SvgPreviewGenerator.generateAll(
-                result.sheets(),
-                result.wastePercents(),
-                request.getContainerWidth(),
-                request.getContainerHeight(),
-                request.getKerf(),
-                request.getPreviewWidth(),
-                request.getPreviewHeight()
-        );
-
-        SvgResponse response = new SvgResponse();
-        response.setSvgs(svgs);
-        response.setWastePercents(result.wastePercents());
-        response.setSheetCount(svgs.size());
-        return response;
-    }
 
 }
